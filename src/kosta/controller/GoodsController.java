1154bf02b9f3a204705caf4fa72d805b63cdb5f6
hsh1112 <hsh1112@gmail.com>
2026-02-09 17:05:32 +0900

    stash로 빼고 pop으로 불러온 후 충돌 해결

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -1,12 +1,15 @@
 package kosta.controller;
 
 public class GoodsController {
-  String s = "바꿨어요";
-  int a = 1;
-  long b = 3;
-  double c = 3.3;
-  String str = "test";
+	String s = "바꿨어요";
+	int a = 1;
+	long b = 3;
+	double c = 3.3;
+	String str = "test";
 
-  public void test(){
-    System.out.println("출력문");
+	public void test() {
+		System.out.println("출력문");
+		System.out.println("출력문 작성중임.. 건드리지 마셈!");
+	}
+
 }
