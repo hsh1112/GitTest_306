7a9d2a45439be032bbfd8373cdb773a77f141d5c
hsh1112 <hsh1112@gmail.com>
2026-02-09 16:41:13 +0900

    메인 출력문 추가

--- src/kosta/view/MainView.java
+++ src/kosta/view/MainView.java
@@ -5,6 +5,7 @@ public class MainView {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello Java!");
+		System.out.println("Hello Kosta!");
 	}
 
 }
